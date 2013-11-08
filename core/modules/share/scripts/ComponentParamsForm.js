d995f8fea521de7dcd12426307e4a871cb70a556
drPavka <d.pavka@gmail.com>
2013-11-08 17:10:38 +0000

    git-svn-id: https://energine.googlecode.com/svn/tags/2.11.8@2714 acdd3125-bb50-0410-93b3-95ffde4b7c7d

--- core/modules/share/scripts/ComponentParamsForm.js
+++ core/modules/share/scripts/ComponentParamsForm.js
@@ -3,18 +3,24 @@ var ComponentParamsForm = new Class({
     Extends: Form,
     initialize: function(el){
         this.parent(el);
-    },
-    save: function(){
-        if (!this.validator.validate()) {
-            return false;
-        }
-        var result = new Hash();
         if(this.codeEditors.length){
             this.codeEditors.each(function(editor){
+                editor.setValue(editor.getValue().replace('<![CDATA['+"\n", '').replace("\n" + ']]>', ''));
+            });
+        }
+    },
+    save: function(){
+        var result = {};
+        if(this.codeEditors.length){
+            this.codeEditors.each(function(editor){
+                editor.setValue('<![CDATA[' + "\n" + editor.getValue() + "\n" +']]>');
                 editor.save();
             });
         }
 
+        if (!this.validator.validate()) {
+            return false;
+        }
         this.form.getElements('input[type=text],input[type=checkbox], select, textarea').each(function(el){
             var value;
             if(el.getProperty('type') == 'checkbox'){
@@ -25,12 +31,12 @@ var ComponentParamsForm = new Class({
             }
 
             if(el.getProperty('name'))
-                result.set(el.getProperty('name'), value);
+                result[el.getProperty('name')] = value;
         });
 
         if(this.richEditors.length){
             this.richEditors.each(function(editor){
-                result.set(editor.hidden.getProperty('name'), editor.area.innerHTML);
+                result[editor.hidden.getProperty('name')] = editor.area.innerHTML;
             });
         }
 
