cc21fa600afb02f5bb1052976e90d3755439f80d
Pavel Dubenko <d.pavka@gmail.com>
2014-09-19 17:09:17 +0300

    result of merge

--- core/modules/share/components/SiteProperties.class.php
+++ core/modules/share/components/SiteProperties.class.php
@@ -7,15 +7,21 @@
  * @author dr.Pavka
  * @copyright Energine 2012
  */
+namespace Energine\share\components;
+use Energine\share\gears\Component;
 
 /**
- * Информация о текущем сайте.
+ * Site variables from "share_sites_properties"
  *
  * @package energine
  * @subpackage trku
  * @author dr.Pavka
  */
 class SiteProperties extends Component {
+    /**
+     * @copydoc Component::defineParams
+     * @return array
+     */
     protected function defineParams() {
         return array_merge(
             parent::defineParams(),
@@ -25,19 +31,22 @@ class SiteProperties extends Component {
         );
     }
 
+    /**
+     *
+     * @return bool
+     */
     public function build() {
         $result = parent::build();
         try {
             if (!$this->getParam('id')) {
-                throw new InvalidArgumentException();
+                throw new \InvalidArgumentException();
             }
             $code = E()->getSiteManager()->getCurrentSite()->{$this->getParam('id')};
             if (!$code) {
-                throw new InvalidArgumentException();
+                throw new \InvalidArgumentException();
             }
-            $result->documentElement->appendChild(new DOMText($code));
-        }
-        catch(InvalidArgumentException $e){
+            $result->documentElement->appendChild(new \DOMText($code));
+        } catch (\InvalidArgumentException $e) {
             $result = false;
         }
         return $result;
